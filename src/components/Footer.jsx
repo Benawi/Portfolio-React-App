d5da357407ff9fec4065cd34a63eeea78b3ca748
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-11-25 16:53:26 +0300

    Add link contact us

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = (props) => {
   const bgStyle = { backgroundColor: "#f5f5f5" };
 
   return (
-    <footer style={bgStyle} className="mt-auto py-5 text-center ">
+    <footer style={bgStyle} id="getInTouch" className="mt-auto py-5 text-center ">
       <Container>
         {props.children}
         <img src={codeImage} alt ="bena"/>  <br></br><br></br>
